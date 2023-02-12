caf7580ac4976a4a7fae4a62fa1b8b457545227f
r2 <ripoll.rodrigo@gmail.com>
2023-02-12 11:23:20 -0300

    start

---
+++ src/main/java/ripoll/challenge/tenpoapi/TenpoApiApplication.java
@@ -0,0 +1,13 @@
+package ripoll.challenge.tenpoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenpoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TenpoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ripoll/challenge/tenpoapi/TenpoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ripoll.challenge.tenpoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TenpoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
